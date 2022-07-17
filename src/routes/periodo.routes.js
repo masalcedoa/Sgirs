f31b0f5c307317eec7f9f78ec8bec1b02b9ccdc8
masalcedoa <42919301+masalcedoa@users.noreply.github.com>
2022-07-16 22:39:51 -0500

    Update periodo.routes.js

--- src/routes/periodo.routes.js
+++ src/routes/periodo.routes.js
@@ -7,20 +7,20 @@ const { isAuthenticated } = require('../helpers/auth');
 
 //New Periodos
 console.log("Ruta con autenticacion");
-router.get('/Periodos/add' ,isAuthenticated, rederPeriodoForm);
+router.get('/periodos/add' ,isAuthenticated, rederPeriodoForm);
 
-router.post('/Periodos/new-Periodo', isAuthenticated, createNewPeriodos);
+router.post('/periodos/new-periodo', isAuthenticated, createNewPeriodos);
 
 //Get all Periodos
-router.get('/Periodos', isAuthenticated, renderPeriodos);
+router.get('/periodos', isAuthenticated, renderPeriodos);
 
 //edit Periodos
-router.get('/Periodos/edit/:id', isAuthenticated, renderEditForm);
-//router.put('/Periodos/edit/:id', updateForm);
+router.get('periodos/edit/:id', isAuthenticated, renderEditForm);
+//router.put('/periodos/edit/:id', updateForm);
 //console.log('nicia update');
-router.put('/Periodos/edit-Periodo/:id',isAuthenticated,  updatePeriodo);
+router.put('/periodos/edit-periodo/:id',isAuthenticated,  updatePeriodo);
 
 //delete Periodos
-router.delete('/Periodos/delete/:id', deletePeriodo);
+router.delete('periodos/delete/:id', deletePeriodo);
 
 module.exports = router;
