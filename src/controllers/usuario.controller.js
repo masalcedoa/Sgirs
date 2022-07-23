bed8f3b3de4f1d4db0a7ad219fb305052dec5834
masalcedoa <42919301+masalcedoa@users.noreply.github.com>
2022-07-23 10:21:30 -0500

    Update usuario.controller.js

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -117,10 +117,14 @@ function actualizar() {
   console.log(value); // en
 }
 
-usersCtrl.renderSigninForm = async (req, res) => {
+usersCtrl.renderSigninForm = async (req, res, User) => {
 
   const lSectores = await sector.find().lean();
   const lPeriodos = await Periodo.find({Estado:true}).lean();
+
+
+
+
 
  // console.log(lSectores);
 
@@ -163,12 +167,130 @@ usersCtrl.SigninValidRest = async (req, res) => {
 
 
 
-  const { correo, codSector, Periodo } = req.body;
+  //const { correo, codSector, Periodo } = req.body;
 
-  const cResult = await User.findOne({ correo: correo, codSector : codSector });
+  const { correo } = req.body;
+
+  //const cResult = await User.findOne({ correo: correo, codSector : codSector });
+
+  const cResult = await User.findOne({ correo: correo});
 
   
   //console.log("Valid Result User:",cResult );
+
+  
+ 
+
+ // const uSession = cResult.correo;
+  
+
+  
+  if (!cResult) {
+    req.flash("success_msg", "Login failure");
+    res.redirect("/usuario/signin");
+
+  } 
+  else {
+
+    const uSession = cResult.correo;
+    
+    //actualiza periodo del usuario
+    //console.log(req.params.id,Periodo);
+   // await User.findOneAndUpdate({correo:correo}, { periodo : Periodo });
+   // console.log("update");
+
+
+
+    req.flash("success_msg", "Login Successfully");
+    res.redirect("/usuario/signinrest");
+  }
+
+
+
+  //res.render("usuario/signin",{lPeriodos, lSectores});
+ 
+ // res.render("usuario/signin");
+
+  //req.flash("success_msg", "Login Successfully");
+  //res.redirect("/");
+};
+
+
+usersCtrl.logout = (req, res) => {
+    /*req.session.User = null;
+    req.session.logout;
+    session.logout;*/
+
+//    res.redirect("/users/signin");
+
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      req.flash("success_msg", "You are logged out now.");
+      res.redirect('/');
+    });
+  
+   // req.logout();
+  //req.logout();
+  //req.flash("success_msg", "You are logged out now.");
+  //res.redirect("/usuario/signin");
+};
+
+usersCtrl.renderSigninFormRest = async (req, res, User) => {
+
+
+  console.log('Rest Validation - Begin - ',req.user);
+  const lSectores = await sector.find({CodSector:req.user.codSector}).lean();
+  const lPeriodos = await Periodo.find({Estado:true}).lean();
+
+ // console.log(lSectores);
+
+  res.render("usuario/signinrest",{lPeriodos, lSectores});
+  
+ 
+  
+};
+
+/*usersCtrl.renderSigninForm = (req, res) => {
+
+  const { correo,password } = req.body;
+  console.log("autenticar antes:", req.body);
+  res.render("usuario/signin");
+};*/
+
+
+usersCtrl.signinrest = passport.authenticate("local", {
+  //successRedirect: "/",
+  failureRedirect: "/usuario/signinrest",
+  failureFlash: true,
+});
+
+
+
+
+/*usersCtrl.signin = async (req, res) => {
+
+
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/usuario/signin",
+    failureFlash: true,
+  });
+
+};*/
+
+
+usersCtrl.SigninValidRestRest = async (req, res) => {
+
+console.log("req body",req.body);
+
+console.log("req user",req.user);
+
+  const { correo, codSector, Periodo } = req.body;
+
+  const cResult = await User.findOne({ correo: req.user.correo, codSector : req.user.codSector });
+
+  
+  console.log("Valid Result User:",cResult );
 
   
  
@@ -208,24 +330,10 @@ usersCtrl.SigninValidRest = async (req, res) => {
 };
 
 
-usersCtrl.logout = (req, res) => {
-    /*req.session.User = null;
-    req.session.logout;
-    session.logout;*/
 
-//    res.redirect("/users/signin");
 
-    req.logout(function(err) {
-      if (err) { return next(err); }
-      req.flash("success_msg", "You are logged out now.");
-      res.redirect('/');
-    });
-  
-   // req.logout();
-  //req.logout();
-  //req.flash("success_msg", "You are logged out now.");
-  //res.redirect("/usuario/signin");
-};
+
+
 
 
 module.exports = usersCtrl;
